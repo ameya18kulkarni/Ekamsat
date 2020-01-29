1f4943cf3b4c6a3ef62efbf7a3f762cc243be1bb
sauravshirwadkar <58627228+sauravshirwadkar@users.noreply.github.com>
2020-01-29 09:50:13 +0530

    Add files via upload

--- ekamsath/about.php
+++ ekamsath/about.php
@@ -15,11 +15,11 @@
 
 
 <!--contact details-->
-<div class="container">
+<!-- <div class="container">
   
   <div class="row ">
   
-    <div class="col-sm">
+    <div class="col-sm ">
     
      <h1>ABOUT VSM</h1>
      <h1>ABOUT VSM</h1>
@@ -32,11 +32,96 @@
      <h1>ABOUT EKAMSATH</h1>
      <h1>ABOUT EKAMSATH</h1>
      <h1>ABOUT EKAMSATH</h1>
-
     </div>
     
   </div>
-</div>
+</div>-->
+
+
+
+<div class="container p-4">
+        <div class="media-container-row ">
+
+            <div class="card p-3 col-8 col-md-6 col-lg-5 d-inline-block">
+                <div class="card-img pb-6">
+                    <span class="mbr-iconfont mbri-users" style="color: rgb(0, 0, 0);" media-simple="true"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5 font-weight-bold text-center">
+                        ABOUT VIVEKANAND SEVA MANDAL</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                    Vivekanand Seva Mandal is an apolitical youth organization inspired by the "Seva" (service) philosophy propagated by the great warrior monk Swami Vivekanand (1863-1902). We are primarily engaged in the areas of youth education and empowerment and development of underprivileged sections of the population.
+As a corollary to the principle of "service to poor is service to god," we at Mandal engage students in welfare activities of tribal -- the most neglected section of our society. The service to poor and to their fellow students develops character and personality in youth and is harnessed for personal, social and the spiritual welfare of the society.
+Mandal has its own functional premises in Dombivli, but it caters to students living in other northern suburbs of Mumbai-Thane region through a 7,000-book-strong library that has been serving professional students of engineering, management and other streams since 1991. We also serve students community with a spacious study center accommodating up to 95 students at a time. The functioning of the library as well as the study center is managed and overseen by a team of student-karyakarte (volunteers) who get hands on training in management of social institutions.
+                    </p>
+                </div>
+            </div>
+
+            
+
+            <div class="card p-3 col-8 col-md-6 col-lg-5 d-inline-block float-right">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-smile-face" style="color: rgb(0, 0, 0);" media-simple="true"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5 font-weight-bold text-center">
+                        ABOUT EKAMSATH
+                    </h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                    Vivekanand Seva Mandal is an apolitical youth organization inspired by the "Seva" (service) philosophy propagated by the great warrior monk Swami Vivekanand (1863-1902). We are primarily engaged in the areas of youth education and empowerment and development of underprivileged sections of the population.
+As a corollary to the principle of "service to poor is service to god," we at Mandal engage students in welfare activities of tribal -- the most neglected section of our society. The service to poor and to their fellow students develops character and personality in youth and is harnessed for personal, social and the spiritual welfare of the society.
+Mandal has its own functional premises in Dombivli, but it caters to students living in other northern suburbs of Mumbai-Thane region through a 7,000-book-strong library that has been serving professional students of engineering, management and other streams since 1991. We also serve students community with a spacious study center accommodating up to 95 students at a time. The functioning of the library as well as the study center is managed and overseen by a team of student-karyakarte (volunteers) who get hands on training in management of social institutions.
+                    </p>    
+                </div>
+            </div>
+         </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+   <!--form-->
+    <div class="container">
+      <h4 class="card-title py-3 mbr-fonts-style display-5 font-weight-bold display-1 text-center">CONTACT US</h4>
+        <div class="row justify-content-center bg-light">
+            <div class="media-container-column col-lg-9" data-form-type="formoid">
+                    <div data-form-alert="" hidden="">
+                        Thanks for filling out the form!
+                    </div>
+            
+                    <form class="mbr-form" action="https://mobirise.com/" method="post" data-form-title="Mobirise Form"><input type="hidden" data-form-email="true" value="R46ImfT8lRYLhZq6vpVE63L0V35bOdmwHjgn18Tp/HdDujD3JfWe/8bm7YjE2vjZZ7JFQMRNRIinYQUh3aNa2qi1GshxArFhHAffNuiFsZiNLfMCF8KElmS7V7lbqBIL">
+                        <div class="row row-sm-offset">
+                            <div class="col-md-4 multi-horizontal" data-for="name">
+                                <div class="form-group">
+                                    <label class="form-control-label mbr-fonts-style display-7" for="name-form1-4 "><p class="font-weight-bold">Name</p></label>
+                                    <input type="text" class="form-control" name="name" data-form-field="Name" required="" id="name-form1-4">
+                                </div>
+                            </div>
+                            <div class="col-md-4 multi-horizontal" data-for="email">
+                                <div class="form-group">
+                                    <label class="form-control-label mbr-fonts-style display-7" for="email-form1-4"><p class="font-weight-bold">Email</p></label>
+                                    <input type="email" class="form-control" name="email" data-form-field="Email" required="" id="email-form1-4">
+                                </div>
+                            </div>
+                            <div class="col-md-4 multi-horizontal" data-for="phone">
+                                <div class="form-group">
+                                    <label class="form-control-label mbr-fonts-style display-7" for="phone-form1-4"><p class="font-weight-bold">Phone</p></label>
+                                    <input type="tel" class="form-control" name="phone" data-form-field="Phone" id="phone-form1-4">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group" data-for="message">
+                            <label class="form-control-label mbr-fonts-style display-7" for="message-form1-4"><p class="font-weight-bold">Message</p></label>
+                            <textarea type="text" class="form-control" name="message" rows="7" data-form-field="Message" id="message-form1-4"></textarea>
+                        </div>
+            
+                        <span class="input-group-btn"><button href="" type="submit" class="btn btn-form btn-black-outline display-4 btn-secondary">SEND FORM</button></span>
+                    </form>
+            </div>
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
  
 
 
